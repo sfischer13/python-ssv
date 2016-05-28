1be1bf26dba956b4ad0e540724d6c18f471724f2
Stefan Fischer <sfischer13@ymail.com>
2016-05-28 12:19:43 +0200

    Add some keywords

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=requirements,
     license='MIT',
     zip_safe=False,
-    keywords='separator delimiter collision CSV TSV SSV',
+    keywords='ASCII collision CSV delimiter separator SSV table text TSV value',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
