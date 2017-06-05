498c3cffe7f09e7abe5ed34021fe026585fe4c74
Stefan Fischer <sfischer13@ymail.com>
2017-06-05 02:26:18 +0200

    Update documentation

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: General',
@@ -49,5 +49,4 @@ setup(
     tests_require=test_requirements,
     url='https://github.com/sfischer13/python-ssv',
     version='0.1.1',
-    zip_safe=False
-)
+    zip_safe=False)
